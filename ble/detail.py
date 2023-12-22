f1984a431810f83197261d2397e1af1170fee14d
weslley <weslley_pablo@hotmail.com.br>
2023-12-22 12:25:00 -0300

    feat: ble detail

--- ble/detail.py
+++ ble/detail.py
@@ -24,7 +24,7 @@ def lambda_handler(event, context):
 
     params = {
         "TableName": environ["BLETagsTable"],
-        "Key": {'petId': {'S': event['pathParameters']['tagMac']}}
+        "Key": {'tagMac': {'S': event['pathParameters']['tagMac']}}
     }
 
     try:
