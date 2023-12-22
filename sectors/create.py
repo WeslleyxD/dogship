8cafc9c28c500f6b2502710b261ed3d78d34042e
weslley <weslley_pablo@hotmail.com.br>
2023-12-22 17:01:17 -0300

    feat: sectors crud

---
+++ sectors/create.py
@@ -0,0 +1,58 @@
+import json
+import boto3
+import logging
+from os import environ
+from uuid import uuid4
+from boto3.dynamodb.types import TypeSerializer
+
+# import requests
+
+logger = logging.getLogger()
+session = boto3.Session()
+ddb = session.client("dynamodb")
+
+
+def lambda_handler(event, context):
+    logger.info(event)
+
+    if event["httpMethod"].upper() not in ("POST", ):
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+               "error": "method invalid",
+            }),
+        } 
+
+    body = json.loads(event["body"]) if "body" in event and event.get("body") else None
+
+    if not body:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+               "error": "body not found",
+            }),
+    }
+
+    try:
+        params = {
+            "TableName": environ["SectorsTable"],
+            "ConditionExpression": "attribute_not_exists(id)",
+            "Item": {k: TypeSerializer().serialize(str(v)) for k,v in body.items()}
+        }
+
+        # Criando um novo item na tabela
+        response = ddb.put_item(**params)
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps("sector created successfully"),
+        }
+
+    except Exception as e:
+        logger.error(e)
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+               "error": str(e),
+            }),
+        }
